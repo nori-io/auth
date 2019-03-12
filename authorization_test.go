e0b239b945b46045c7f6a11aa93fd8e73bd6fa7d
Sergey <serge@cheebo.me>
2019-03-12 18:01:19 +0500

    mock example

---
+++ authorization_test.go
@@ -0,0 +1,54 @@
+package main_test
+
+import (
+	"context"
+	"testing"
+
+	auth "github.com/nori-io/auth"
+	config2 "github.com/nori-io/nori-common/config"
+	"github.com/nori-io/nori-common/meta"
+	"github.com/nori-io/nori-common/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestPlugin_Start(t *testing.T) {
+	a := assert.New(t)
+
+	plugin := auth.Plugin
+
+	sDescr := mock.MatchedBy(func(key string) bool {
+		return len(key) >= 0
+	})
+
+	// mock
+	config := mocks.Config{}
+	config.On("String", "jwt.sub", sDescr).Return(func() config2.String {
+		return func() string {
+			return "jwt.sub"
+		}
+	}())
+	config.On("String", "jwt.iss", sDescr).Return(func() config2.String {
+		return func() string {
+			return "jwt.iss"
+		}
+	}())
+	configManager := &mocks.Manager{}
+	configManager.On("Register", mock.MatchedBy(func(m meta.Meta) bool {
+		return true
+	})).Return(&config)
+
+	//
+	auth := &mocks.Auth{}
+	auth.On("")
+
+	//
+	registry := &mocks.Registry{}
+	registry.On("Auth").Return(auth)
+
+	//
+	ctx := context.Background()
+	plugin.Init(ctx, configManager)
+	err := plugin.Start(ctx, registry)
+	a.Nil(err)
+}

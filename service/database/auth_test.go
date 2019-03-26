8f465449ab86103ab597ea047389dfb884dc99c6
anita <bruteforce1414@gmail.com>
2019-03-26 15:42:11 +0500

    Reduce code in test

--- service/database/auth_test.go
+++ service/database/auth_test.go
@@ -1,7 +1,6 @@
 package database_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -15,53 +14,37 @@ func TestAuth_FindByEmail(t *testing.T) {
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
-//	defer mockDatabase.Close()
-//	defer mock.ExpectClose()
-
-	mock.ExpectBegin()
-
-	mock.ExpectExec("INSERT INTO users (status_account, type, created, updated,mfa_type) VALUES(?,?,?,?,?)").
-		WithArgs("active", "vendor", AnyTime{}, AnyTime{}).WillReturnResult(sqlmock.NewResult(1, 1))
-
-	rows := sqlmock.NewRows([]string{"id"}).
-		AddRow(1).
-		RowError(1, fmt.Errorf("row error"))
-	mock.ExpectQuery("SELECT LAST_INSERT_ID()").WillReturnRows(rows)
-
-	mock.ExpectExec("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)").
-		WithArgs(1, "test@mail.ru", "pass", "salt", AnyTime{}, AnyTime{}, false, false).WillReturnResult(sqlmock.NewResult(1, 1))
-    mock.ExpectCommit()
-
-	/*mock.ExpectPrepare("INSERT INTO users (status_account, type, created, updated,mfa_type) VALUES(?,?,?,?,?)").ExpectExec().
-		WithArgs("active", "vendor", AnyTime{}, AnyTime{}).WillReturnResult(sqlmock.NewResult(1, 1))
-
-
-
-
-	mock.ExpectPrepare("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)").ExpectExec().
-		WithArgs(1, "test@mail.ru", "pass", "salt", AnyTime{}, AnyTime{}, false, false).WillReturnResult((sqlmock.NewResult(1, 1)))
-*/
 
 	nonEmptyRows := sqlmock.NewRows([]string{"id", "email", "password"}).
-		AddRow(1,"test@mail.ru", "pass")
+		AddRow(1, "test@mail.ru", "pass")
 
 	mock.ExpectQuery("SELECT id, email,password FROM auth WHERE email = ? LIMIT 1").WithArgs("test@mail.ru").WillReturnRows(nonEmptyRows)
 
-    d := database.DB(mockDatabase,logrus.New())
+	d := database.DB(mockDatabase, logrus.New())
 
+	_, err = d.Auth().FindByEmail("test@mail.ru")
 
-	err = d.Users().Create(&database.AuthModel{
-		Email:    "test@mail.ru",
-		Password: "pass",
-		Salt:     "salt",
-	}, &database.UsersModel{
-		Status_account: "active",
-		Type:           "vendor",
-	})
-	if err != nil {
-		t.Error(err)
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
-	_,err = d.Auth().FindByEmail("test@mail.ru")
+
+}
+
+func TestAuth_FindByPhone(t *testing.T) {
+	mockDatabase, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	nonEmptyRows := sqlmock.NewRows([]string{"id", "phone_country_code", "password"}).
+		AddRow(1, "test@mail.ru", "pass")
+
+	mock.ExpectQuery("SELECT id, email,password FROM auth WHERE email = ? LIMIT 1").WithArgs("test@mail.ru").WillReturnRows(nonEmptyRows)
+
+	d := database.DB(mockDatabase, logrus.New())
+
+	_, err = d.Auth().FindByEmail("test@mail.ru")
 
 	// we make sure that all expectations were met
 	if err := mock.ExpectationsWereMet(); err != nil {

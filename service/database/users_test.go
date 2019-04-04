1faa695913bb720a72f92582207d6fe4c14a95b4
anita <bruteforce1414@gmail.com>
2019-04-04 18:06:23 +0500

    Fix bag in test by replacing mock.ExpectBegin

--- service/database/users_test.go
+++ service/database/users_test.go
@@ -17,25 +17,28 @@ type (
 )
 
 func TestUsers_Create_userEmail(t *testing.T) {
-	mockDatabase, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+
+	mockDatabase, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
-	defer mockDatabase.Close()
-	defer mock.ExpectClose()
-	mock.ExpectBegin()
+
+	//mock.ExpectBegin()
 	/*mock.ExpectExec("INSERT INTO users (status_account, type, created, updated) VALUES(?,?,?,?)").
 		WithArgs("active", "vendor", AnyTime{}, AnyTime{}).WillReturnResult(sqlmock.NewResult(1, 1))*/
 
-	mock.ExpectPrepare("INSERT INTO").ExpectExec().WithArgs("active", "vendor", AnyTime{}, AnyTime{}).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectPrepare("INSERT INTO").
+		ExpectExec().WithArgs("active", "vendor", AnyTime{}, AnyTime{}).
+		WillReturnResult(sqlmock.NewResult(1, 1))
 
+    mock.ExpectBegin()
 	stmt, err := mockDatabase.Prepare("INSERT INTO users (status_account, type, created, updated) VALUES(?,?,?,?)")
 	if err != nil {
-		panic(err)
+		t.Log(err)
 	}
-
 	if _, err := stmt.Exec("active", "vendor", AnyTime{}, AnyTime{}); err != nil {
-		t.Fatal(err)
+		mock.ExpectRollback()
+		t.Log(err)
 	}
 
 
@@ -44,8 +47,19 @@ func TestUsers_Create_userEmail(t *testing.T) {
 		RowError(1, fmt.Errorf("row error"))
 	mock.ExpectQuery("SELECT LAST_INSERT_ID()").WillReturnRows(rows)
 
-	mock.ExpectExec("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)").
-		WithArgs(20, "users_create_email_test@mail.ru", "users_create_email_pass", "users_create_email_salt", AnyTime{}, AnyTime{}, false, false).WillReturnResult(sqlmock.NewResult(20, 1))
+	mock.ExpectPrepare("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)").
+		ExpectExec().WithArgs(20, "users_create_email_test@mail.ru", "users_create_email_pass", "users_create_email_salt", AnyTime{}, AnyTime{}, false, false).WillReturnResult(sqlmock.NewResult(20, 1))
+	stmt, err = (mockDatabase.Prepare("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)"))
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := stmt.Exec(20, "users_create_email_test@mail.ru", "users_create_email_pass", "users_create_email_salt", AnyTime{}, AnyTime{}, false, false); err != nil {
+		t.Fatal(err)
+	}
+
+	mock.ExpectExec("INSERT INTO auth (user_id,  email, password, salt, created, updated, is_email_verified, is_phone_verified) VALUES(?,?,?,?,?,?,?,?)")
+
 	mock.ExpectCommit()
 
 	d := database.DB(mockDatabase, logrus.New())
@@ -66,7 +80,10 @@ func TestUsers_Create_userEmail(t *testing.T) {
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
+	defer stmt.Close()
 
+	defer mockDatabase.Close()
+	defer mock.ExpectClose()
 }
 
 func TestUsers_Create_userPhone(t *testing.T) {

e73079b997ab0e2b293b0bec3a9f9a50bf0bee50
anita <bruteforce1414@gmail.com>
2019-04-30 18:21:04 +0500

    Delete unnecessary package for generating letters

--- service/database/mfa_code.go
+++ service/database/mfa_code.go
@@ -3,10 +3,8 @@ package database
 import (
 	"context"
 	"database/sql"
-	"math/rand"
-	"time"
-	"unsafe"
 
+	"github.com/cheebo/rand"
 	"github.com/nori-io/nori-common/interfaces"
 )
 
@@ -21,7 +19,6 @@ const (
 	mask     = 1<<6 - 1
 )
 
-var rng = rand.NewSource(time.Now().UnixNano())
 
 func (c *mfaCode) Create(modelMfaCode *MfaCodeModel) ([]string, error) {
 
@@ -35,7 +32,7 @@ func (c *mfaCode) Create(modelMfaCode *MfaCodeModel) ([]string, error) {
 	_, execErr := tx.Exec("DELETE FROM user_mfa_code WHERE user_id=?", modelMfaCode.UserId)
 
 	for index := 0; index < 10; index++ {
-		generatedCode := RandStr(5) + "-" + RandStr(5)
+		generatedCode := rand.RandomString(5) + "-" + rand.RandomString(5)
 		_, execErr = tx.Exec("INSERT INTO user_mfa_code (user_id, code) VALUES(?,?)",
 			modelMfaCode.UserId, generatedCode)
 		if len(generatedCode) != 0 {
@@ -76,16 +73,4 @@ func (c *mfaCode) Delete(code string) error {
 
 }
 
-func RandStr(ln int) string {
-	buf := make([]byte, ln)
-	for idx, cache, remain := ln-1, rng.Int63(), 10; idx >= 0; {
-		if remain == 0 {
-			cache, remain = rng.Int63(), 10
-		}
-		buf[idx] = chars[int(cache&mask)%charsLen]
-		cache >>= 6
-		remain--
-		idx--
-	}
-	return *(*string)(unsafe.Pointer(&buf))
-}
+

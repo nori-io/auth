3e117e2376383e6a4a074d2fb5bb3f3c83dee43f
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-30 18:15:38 +0500

    refactor: imports were fixed

--- plugin/main.go
+++ plugin/main.go
@@ -2,67 +2,51 @@ package main
 
 import (
 	"context"
+
 	"github.com/jinzhu/gorm"
+	"github.com/nori-io/common/v3/pkg/domain/config"
 	"github.com/nori-io/common/v3/pkg/domain/logger"
 	"github.com/nori-io/common/v3/pkg/domain/meta"
-	"github.com/nori-io/common/v3/pkg/domain/registry"
-	noriGorm "github.com/nori-io/interfaces/public/sql/gorm"
-	m "github.com/nori-io/common/v3/pkg/meta"
 	p "github.com/nori-io/common/v3/pkg/domain/plugin"
-	"github.com/nori-io/common/v3/pkg/domain/config"
+	"github.com/nori-io/common/v3/pkg/domain/registry"
+	m "github.com/nori-io/common/v3/pkg/meta"
+	noriGorm "github.com/nori-io/interfaces/public/sql/gorm"
 )
+
 var (
 	Plugin p.Plugin = plugin{}
 )
 
-
 type plugin struct {
-	db *gorm.DB
+	db     *gorm.DB
 	config conf
-
 }
 
 type conf struct {
 	Sub config.String
 	Iss config.String
-
 }
 
 func (p plugin) Meta() meta.Meta {
 	return m.Meta{
-		ID: meta.ID{
-			ID:      "nori/authorization",
-			Version: "1.0.0",
+		ID: m.ID{
+			ID:      "",
+			Version: "",
 		},
-		Author: meta.Author{
-			Name: "Nori",
-			URI:  "https://noricms.com",
-		},
-		Core: meta.Core{
-			VersionConstraint: ">=1.0.0, <2.0.0",
-		},
-		Dependencies: []meta.Dependency{
-			meta.HTTP.Dependency("1.0.0"),
-			meta.SQL.Dependency("1.0.0"),
-		},
-		Description: meta.Description{
-			Name:        "NoriCMS Naive Posts Plugin",
-			Description: "Naive Posts Plugin",
-		},
-		Interface: meta.Custom,
-		License: meta.License{
-			Title: "",
-			Type:  "GPLv3",
-			URI:   "https://www.gnu.org/licenses/",
-		},
-		Tags: []string{"cms", "posts", "api"},
+		Author:       nil,
+		Dependencies: nil,
+		Description:  nil,
+		Interface:    "",
+		License:      nil,
+		Links:        nil,
+		Repository:   nil,
+		Tags:         nil,
 	}
 }
 
 func (p plugin) Instance() interface{} {
 	return p.db
 }
-
 
 func (p plugin) Init(ctx context.Context, config config.Config, log logger.FieldLogger) error {
 	p.config = conf{
@@ -74,99 +58,99 @@ func (p plugin) Init(ctx context.Context, config config.Config, log logger.Field
 
 func (p plugin) Start(ctx context.Context, registry registry.Registry) error {
 
-	p.db, err := noriGorm.GetGorm(registry)
+	p.db, _ = noriGorm.GetGorm(registry)
 
 	/*if p.instance == nil {
 
-		http, err := registry.Http()
-		if err != nil {
-			return err
-		}
+	http, err := registry.Http()
+	if err != nil {
+		return err
+	}
 
-		session, err := registry.Session()
-		if err != nil {
-			return err
-		}
+	session, err := registry.Session()
+	if err != nil {
+		return err
+	}
 
-		db, err := registry.Sql()
-		if err != nil {
-			return err
-		}*/
+	db, err := registry.Sql()
+	if err != nil {
+		return err
+	}*/
 
-		/*p.instance = service.NewService(
-			auth,
-			session,
-			p.config,
-			registry.Logger(p.Meta()),
-			database.DB(db.GetDB()),
-		)
-		service.Transport(auth, transport, session,
-			http, p.instance, registry.Logger(p.Meta()))*/
+	/*p.instance = service.NewService(
+		auth,
+		session,
+		p.config,
+		registry.Logger(p.Meta()),
+		database.DB(db.GetDB()),
+	)
+	service.Transport(auth, transport, session,
+		http, p.instance, registry.Logger(p.Meta()))*/
 
-/*		sql1, err := registry.Sql()
-		if err != nil {
-			return err
-		}
-		db1 := sql1.GetDB()
+	/*		sql1, err := registry.Sql()
+			if err != nil {
+				return err
+			}
+			db1 := sql1.GetDB()
 
-		tx, err := db1.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+			tx, err := db1.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, execErr := tx.Exec(
-			sqlScripts.CreateTableUsers)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, execErr := tx.Exec(
+				sqlScripts.CreateTableUsers)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
 
-		}
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableAuth)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableAuthProviders)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
+			}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableAuth)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableAuthProviders)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
 
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableAuthenticationHistory)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableAuthenticationHistory)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
 
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableUserMfaPhone)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableUserMfaPhone)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
 
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableUsersMfaCode)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
-		_, execErr = tx.Exec(
-			sqlScripts.CreateTableUserMfaSecret)
-		if execErr != nil {
-			_ = tx.Rollback()
-			log.Fatal(execErr)
-		}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableUsersMfaCode)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
+			_, execErr = tx.Exec(
+				sqlScripts.CreateTableUserMfaSecret)
+			if execErr != nil {
+				_ = tx.Rollback()
+				log.Fatal(execErr)
+			}
 
-		if err := tx.Commit(); err != nil {
-			log.Fatal(err)
-		}
+			if err := tx.Commit(); err != nil {
+				log.Fatal(err)
+			}
 
-		/*service.Transport(auth, transport, session,
-			http, p.instance, registry.Logger(p.Meta()))*/
+			/*service.Transport(auth, transport, session,
+				http, p.instance, registry.Logger(p.Meta()))*/
 	//}
 	return nil
 }
@@ -174,7 +158,6 @@ func (p plugin) Start(ctx context.Context, registry registry.Registry) error {
 func (p plugin) Stop(ctx context.Context, registry registry.Registry) error {
 	return nil
 }
-
 
 /*
 func (p plugin) Install(_ context.Context, registry noriPlugin.Registry) error {
@@ -193,7 +176,7 @@ func (p plugin) UnInstall(_ context.Context, registry noriPlugin.Registry) error
 		return err
 	}
 	db := sql.GetDB()
-	_, err = db.Exec(` 
+	_, err = db.Exec(`
 		drop table articles;
 		drop table comments;
 		`)

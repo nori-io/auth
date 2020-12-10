52f3979e68a52f2e2f45882f35ff4302cefdb622
bruteforce1414 <bruteforce1414@gmail.com>
2020-12-10 13:15:50 +0500

    refactor: remove unused sql scripts' files

--- asserts/sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-(id bigserial PRIMARY KEY,
- email  VARCHAR (32) NOT NULL,
- password VARCHAR (32) NOT NULL,
- status   SMALLINT NOT NULL,
- created_at TIMESTAMP,
- updated_at TIMESTAMP);
-

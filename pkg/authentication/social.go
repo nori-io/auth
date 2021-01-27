296ae74872723a97769dbc021060460c321fef86
bruteforce1414 <bruteforce1414@gmail.com>
2021-01-27 15:14:16 +0500

    feat: GetUserSocialAccounts()

--- pkg/authentication/social.go
+++ pkg/authentication/social.go
@@ -3,15 +3,27 @@ package authentication
 import "context"
 
 type Social interface {
-	GetUserSocialAccounts(ctx context.Context, userID uint64) []SocialAccount
+	GetUserSocialAccounts(ctx context.Context, userProfile UserProfile) []SocialAccount
 	IsValid(ctx context.Context, tokenAccess string) bool
-
 	RefreshToken(ctx context.Context, tokenRefresh string) (tokenAccess string)
 }
 
 type SocialAccount struct {
-	name        string
-	logo        string
-	authApi     string
-	callBackApi string
+	UserProfile   UserProfile
+	SocialNetwork SocialNetwork
+}
+
+type UserProfile struct {
+	ID        uint64
+	FirstName string
+	LastName  string
+	FullName  string
+	Email     string
+	AvatarURL string
+	Raw       string
+}
+
+type SocialNetwork struct {
+	name string
+	logo string
 }

e2889c1245aa7e29c9933347544791f7e2a1878e
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-15 15:56:22 +0500

    feat: model for one_time_token

--- internal/repository/one_time_token/postgres/model.go
+++ internal/repository/one_time_token/postgres/model.go
@@ -1,1 +1,13 @@
 package postgres
+
+import "time"
+
+type model struct {
+	ID        uint64    `gorm:"column:id; PRIMARY_KEY; type:bigserial"`
+	UserID    uint64    `gorm:"column:user_id; type: bigint; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Issuer    string    `gorm:"column:issuer; type:VARCHAR(64)"`
+	Token     string    `gorm:"column:token; type:VARCHAR(254)"`
+	TTL       time.Time `gorm:"column:ttl; type: TIMESTAMP; not null"`
+	CreatedAt time.Time `gorm:"column:created_at; type: TIMESTAMP; not null"`
+	UsedAt    time.Time `gorm:"column:used_at; type: TIMESTAMP"`
+}

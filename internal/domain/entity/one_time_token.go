4985297d7fd3fed5f438fe9ff050970c3b6578b6
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-15 16:29:51 +0500

    feat: One Time Token model: Convert() and NewModel()

--- internal/domain/entity/one_time_token.go
+++ internal/domain/entity/one_time_token.go
@@ -3,11 +3,12 @@ package entity
 import "time"
 
 type OneTimeToken struct {
-	ID        uint64
-	UserID    uint64
-	Issuer    string
-	Token     []byte
-	TTL       time.Duration
+	ID     uint64
+	UserID uint64
+	Issuer string
+	Token  []byte
+	// @todo time.duration? for ttl
+	TTL       time.Time
 	CreatedAt time.Time
 	UsedAt    time.Time
 }

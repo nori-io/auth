e985d5c335018fde109160c9be949bbe334cc153
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-15 12:43:06 +0500

    feat: fill constructor for all repositories

--- internal/repository/constructor.go
+++ internal/repository/constructor.go
@@ -3,22 +3,37 @@ package repository
 import "github.com/nori-plugins/authentication/internal/domain/repository"
 
 type Repository struct {
-	MfaRecoveryCodeRepository repository.MfaRecoveryCodeRepository
-	MfaSecretRepository       repository.MfaSecretRepository
-	UserRepository            repository.UserRepository
+	AuthenticationHistoryRepository repository.AuthenticationHistoryRepository
+	MfaRecoveryCodeRepository       repository.MfaRecoveryCodeRepository
+	MfaSecretRepository             repository.MfaSecretRepository
+	OneTimeTokenRepository          repository.OneTimeTokenRepository
+	ServiceProviderRepository       repository.ServiceProviderRepository
+	SessionRepository               repository.SessionRepository
+	SocialAccountRepository         repository.SocialAccountRepository
+	UserRepository                  repository.UserRepository
 }
 
 type Params struct {
-	MfaRecoveryCodeRepository repository.MfaRecoveryCodeRepository
-	MfaSecretRepository       repository.MfaSecretRepository
-	UserRepository            repository.UserRepository
+	AuthenticationHistoryRepository repository.AuthenticationHistoryRepository
+	MfaRecoveryCodeRepository       repository.MfaRecoveryCodeRepository
+	MfaSecretRepository             repository.MfaSecretRepository
+	OneTimeTokenRepository          repository.OneTimeTokenRepository
+	ServiceProviderRepository       repository.ServiceProviderRepository
+	SessionRepository               repository.SessionRepository
+	SocialAccountRepository         repository.SocialAccountRepository
+	UserRepository                  repository.UserRepository
 }
 
 func New(params Params) *Repository {
 	repository := Repository{
-		MfaRecoveryCodeRepository: params.MfaRecoveryCodeRepository,
-		MfaSecretRepository:       params.MfaSecretRepository,
-		UserRepository:            params.UserRepository,
+		AuthenticationHistoryRepository: params.AuthenticationHistoryRepository,
+		MfaRecoveryCodeRepository:       params.MfaRecoveryCodeRepository,
+		MfaSecretRepository:             params.MfaSecretRepository,
+		OneTimeTokenRepository:          params.OneTimeTokenRepository,
+		ServiceProviderRepository:       params.ServiceProviderRepository,
+		SessionRepository:               params.SessionRepository,
+		SocialAccountRepository:         params.SocialAccountRepository,
+		UserRepository:                  params.UserRepository,
 	}
 	return &repository
 }

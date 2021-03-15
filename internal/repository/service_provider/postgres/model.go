d2a415465aea6b59ceedd57b9951aa7264733826
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-15 16:59:51 +0500

    feat: Service Provider model: Convert() and NewModel()

--- internal/repository/service_provider/postgres/model.go
+++ internal/repository/service_provider/postgres/model.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "time"
+import (
+	"time"
+
+	"github.com/nori-plugins/authentication/internal/domain/entity"
+)
 
 type model struct {
 	ID        uint64    `gorm:"column:id; PRIMARY_KEY; type:bigserial"`
@@ -8,4 +12,29 @@ type model struct {
 	Logo      string    `gorm:"column:logo; type:VARCHAR(254)"`
 	CreatedAt time.Time `gorm:"column:created_at; type: TIMESTAMP; not null"`
 	UpdatedAt time.Time `gorm:"column:updated_at; type: TIMESTAMP"`
+}
+
+func (m model) Convert() *entity.ServiceProvider {
+	return &entity.ServiceProvider{
+		ID:        m.ID,
+		Name:      m.Name,
+		Logo:      m.Logo,
+		CreatedAt: m.CreatedAt,
+		UpdatedAt: m.UpdatedAt,
+	}
+}
+
+func NewModel(e *entity.ServiceProvider) model {
+	return model{
+		ID:        e.ID,
+		Name:      e.Name,
+		Logo:      e.Logo,
+		CreatedAt: e.CreatedAt,
+		UpdatedAt: e.UpdatedAt,
+	}
+}
+
+// TableName
+func (model) TableName() string {
+	return "service_providers"
 }

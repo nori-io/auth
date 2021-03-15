4246033f55132880a75d1cd26bab5edfb7bf9dba
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-15 17:11:06 +0500

    feat: model for session

--- internal/repository/session/postgres/model.go
+++ internal/repository/session/postgres/model.go
@@ -1,1 +1,13 @@
 package postgres
+
+import "time"
+
+type model struct {
+	ID         uint64    `gorm:"column:id; PRIMARY_KEY; type:bigserial"`
+	UserID     uint64    `gorm:"column:user_id; type: bigint; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	SessionKey string    `gorm:"column:session_key; type:VARCHAR(128); not null"`
+	Status     uint8     `gorm:"column:status; type:smallint; not null"`
+	OpenedAt   time.Time `gorm:"column:opened_at; type: TIMESTAMP; not null"`
+	ClosedAt   time.Time `gorm:"column:closed_at; type: TIMESTAMP"`
+	UpdatedAt  time.Time `gorm:"column:updated_at; type: TIMESTAMP"`
+}

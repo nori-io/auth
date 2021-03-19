538025d54badd9f5950d502552f4686dd9fa77bf
bruteforce1414 <bruteforce1414@gmail.com>
2021-03-19 18:51:27 +0500

    feat: describe settings interface

--- internal/domain/service/settings.go
+++ internal/domain/service/settings.go
@@ -1,4 +1,9 @@
 package service
 
+import "context"
+
 type SettingsService interface {
+	ReceiveMfaStatus(ctx context.Context, sessionKey string)
+	DisableMfa(ctx context.Context, sessionKey string)
+	ChangePassword(ctx context.Context, sessionKey string, passwordOld string, passwordNew string)
 }
